9c5b927c771e813b0de0e2c22eca18d76d5f4058
rohit0987 <rohithamlpur@gmail.com>
2024-01-23 09:11:09 +0530

    Initial commit

---
+++ server/controllers/client.js
@@ -0,0 +1,105 @@
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
+import getCountryIso3 from "country-iso-2-to-3";
+
+// ... (other imports)
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    
+    const productsWithStats = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({
+          productId: product._id,
+        });
+        return {
+          ...product._doc,
+          stat,
+        };
+      })
+    );
+
+    res.status(200).json(productsWithStats);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export const getTransactions = async (req, res) => {
+  try {
+    const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+
+    const generateSort = (sort) => {
+      if (!sort) return {};
+      const sortParsed = JSON.parse(sort);
+      return { [sortParsed.field]: sortParsed.sort === 'asc' ? 1 : -1 };
+    };
+
+    const sortFormatted = generateSort(sort);
+
+    const transactions = await Transaction.find({
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
+    })
+      .sort(sortFormatted)
+      .skip(page * pageSize)
+      .limit(pageSize);
+
+    const total = await Transaction.countDocuments({
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
+    });
+
+    res.status(200).json({
+      transactions,
+      total,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export const getGeography = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    const mappedLocations = users.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    }, {});
+
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
+
+    res.status(200).json(formattedLocations);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
